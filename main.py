aad33f415639b14556d71cd7ae6468938043faa5
KateiRen <Karsten.Hartlieb@gmx.de>
2021-06-21 22:46:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ import usocket as socket
 
 webradios = [["Das Ding","swr-dasding-live.cast.addradio.de", "/swr/dasding/live/mp3/128/stream.mp3", 80],
              ["Bayern 3","br-br3-live.cast.addradio.de","/br/br3/live/mp3/128/stream.mp3",80],
-             ["FM4","mp3stream1.apasf.apa.at","/", 8000],
+             ["FM4","mp3stream1.apasf.apa.at","/listen.pls", 8000],
              ["egofm","egofm-live.cast.addradio.de","/egofm/live/mp3/high/stream.mp3",80],
              ["Kosmos","radiostreaming.ert.gr","/ert-kosmos",80],
              ["egofm","egofm-ais-edge-400b-dus-dtag-cdn.cast.addradio.de","/egofm/live/mp3/high/stream.mp3?_art=dj0yJmlwPTkxLjQ5LjM2LjE1NiZpZD1pY3NjeGwtd2pub25jbm1iJnQ9MTYyNDA5ODA3MSZzPTc4NjZmMjljI2E5NDQ2ODczZWExYjY5ZDY1ZTlhOTEyNjFiYTBjZWEw",80]]
 
 
-
-station = 3
-
-print("Playing: "+webradios[station][0])
-print(webradios[station][1])
-print(webradios[station][2])
-print(webradios[station][3])
+station = 2
 
 spi = SPI(1, vs10xx.SPI_BAUDRATE) # SPI bus id=1 pinout: SCK = 14, MOSI = 13, MISO = 12
 
@@ -32,32 +26,52 @@ player = vs10xx.Player(
 print("Player set up")
 player.setVolume(0.8) # the range is 0 to 1.0
 
-    
-addr = socket.getaddrinfo(webradios[station][1], webradios[station][3])[0][-1]
-print(addr)
-s = socket.socket()
-s.connect(addr)
+s = ""
+buf = ""
 
-s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (webradios[station][2], webradios[station][1]), 'utf8'))
-# print(s.recv(3200))
+def connectAndTry():
+    print("Station Name: " +webradios[station][0])
+    print("Station Host: " + webradios[station][1])
+    print("Station Path: " + webradios[station][2])
+    print("Station Port: " + str(webradios[station][3]))
 
+    global s, buf
+    s = socket.socket()
+    addr = socket.getaddrinfo(webradios[station][1], webradios[station][3])[0][-1]
+    print(addr)
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (webradios[station][2], webradios[station][1]), 'utf8'))
 
-# eigenes Array einsetzen uns peux a peux füllen...
-
-buf = s.recv(320)
-print(buf)
-print(buf[9:12])
-if buf[9:15] == b'200 OK':
-    print("OK start streaming right away")
-    # optionally skip ahead until mp3 stream is reached
-    buf = s.recv(32)
-elif buf[9:18] == b'302 Found':
-    print("Need to extract redirect address first")
-else:
-    print("no valid signature found")
+    buf = s.recv(1000)
     print(buf)
-    buf = None
+    print(buf[9:15])
+    if buf[9:15] == b'200 OK':
+        print("OK start streaming")
+        # optionally skip ahead until mp3 stream is reached
+        return "OK"
+    elif buf[9:18] == b'302 Found':
+        s.close()
+        print("Need to extract redirect address first")
+        start = str(buf).find("Location: ") + 10 # "Location: " mit überspringen
+        redirect = str(buf)[start:-9] # \r\n\r\n am Ende abschneiden
+        print(redirect)
+        webradios[station][1] = redirect.split("/")[2]
+        webradios[station][2] = redirect[len(webradios[station][1])+7:]
+        print(webradios[station][1])
+        print(webradios[station][2])
+        return "Redirect"
+    else:
+        print("no valid signature found")
+        print(buf)
+        return "Fail"
 
+
+if connectAndTry() == "OK":
+    buf = s.recv(32)
+elif connectAndTry() == "OK": # 2nd try, in the meantime the station host and path have been replaced with redirect data
+    buf = s.recv(32)
+else:
+    buf = None
 
 while buf:
     player.writeData(buf)
