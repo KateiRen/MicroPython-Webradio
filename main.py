c691405bdb3540d0afa538a6d8fb40363c8830fb
Katei <Karsten.Hartlieb@gmx.de>
2021-06-21 13:04:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,3 +64,19 @@ while True:
         break
 
 s.close()
+
+
+
+# This code snippet is not optimized
+# now = time.ticks_ms()
+# scheduled_time = task.scheduled_time()
+# if ticks_diff(scheduled_time, now) > 0:
+#     print("Too early, let's nap")
+#     sleep_ms(ticks_diff(scheduled_time, now))
+#     task.run()
+# elif ticks_diff(scheduled_time, now) == 0:
+#     print("Right at time!")
+#     task.run()
+# elif ticks_diff(scheduled_time, now) < 0:
+#     print("Oops, running late, tell task to run faster!")
+#     task.run(run_faster=true)
